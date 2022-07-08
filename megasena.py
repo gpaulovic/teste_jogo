003b4add0500d9851ed9e1fc5053554237b25613
gpaulovic <93664354+gpaulovic@users.noreply.github.com>
2022-07-08 10:07:15 -0300

    Add files via upload

---
+++ megasena.py
@@ -0,0 +1,31 @@
+from random import randint
+from time import sleep
+lista = []
+jogos = []
+print('-'*30)
+print('      jogos da MEGA SENA      ')
+print('-'*30)
+quant = int(input('Quantos jogos quer fazer?: '))
+total = 1
+while total <= quant:
+    cont = 0
+    while True:
+        num = randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont +=1
+        if cont >=6:
+            break
+    lista.sort()
+    jogos.append(lista [:])
+    lista.clear()
+    total +=1
+print('-='*3, f'Sorteando {quant} jogos', '-='*3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(0.5)
+print('-='*5, '< Boa sorte! >', '-='*5)
+
+
+    
+
