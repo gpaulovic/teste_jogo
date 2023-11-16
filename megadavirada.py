7b40e2b9494daae2b9c782f2a1f63261e536d94f
Glauber Paulovic <glauberpaulovic@gmail.com>
2023-11-16 10:06:25 -0300

    adicionar .gitignore e atualizacao dos jogos para criar db

--- megadavirada.py
+++ megadavirada.py
@@ -3,7 +3,7 @@ from random import randint
 from time import sleep
 
 def create_table():
-    conn = sqlite3.connect('lotomania.db')
+    conn = sqlite3.connect('megadavirada.db')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS jogos (
@@ -15,30 +15,29 @@ def create_table():
     conn.close()
 
 def save_jogo(jogo):
-    conn = sqlite3.connect('lotomania.db')
+    conn = sqlite3.connect('megadavirada.db')
     cursor = conn.cursor()
     cursor.execute('INSERT INTO jogos (numeros) VALUES (?)', (' '.join(map(str, jogo)),))
     conn.commit()
     conn.close()
 
 def check_duplicate(jogo):
-    conn = sqlite3.connect('lotomania.db')
+    conn = sqlite3.connect('megadavirada.db')
     cursor = conn.cursor()
     cursor.execute('SELECT COUNT(*) FROM jogos WHERE numeros = ?', (' '.join(map(str, jogo)),))
     count = cursor.fetchone()[0]
     conn.close()
     return count > 0
 
-def imprime_matriz(matriz):
-    for linha in matriz:
-        print(linha)
+def imprime_lista(jogo):
+    print(', '.join(map(str, jogo)))
 
 lista = []
 jogos = []
 create_table()
 
 print('-'*30)
-print('      jogos da LOTOMANIA      ')
+print('    jogos da MEGA DA VIRADA    ')
 print('-'*30)
 quant = int(input('Quantos jogos quer fazer?: '))
 total = 1
@@ -46,11 +45,11 @@ total = 1
 while total <= quant:
     cont = 0
     while True:
-        num = randint(1, 100)
+        num = randint(1, 60)
         if num not in lista:
             lista.append(num)
             cont += 1
-        if cont >= 50:
+        if cont >= 6:
             break
     lista.sort()
 
@@ -63,8 +62,7 @@ while total <= quant:
 
 print('-='*3, f'Sorteando {quant} jogos', '-='*3)
 for i, l in enumerate(jogos):
-    matriz_jogo = [l[i:i+10] for i in range(0, len(l), 10)]
-    imprime_matriz(matriz_jogo)
+    imprime_lista(l)
     sleep(0.5)
 
-print('-='*5, '< Boa sorte! >', '-='*5)
+print('-='*5, '< Boa sorte na MEGA DA VIRADA! >', '-='*5)
--- megasena.py
+++ megasena.py
@@ -1,31 +1,68 @@
+import sqlite3
 from random import randint
 from time import sleep
+
+def create_table():
+    conn = sqlite3.connect('megasena.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS jogos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            numeros TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def save_jogo(jogo):
+    conn = sqlite3.connect('megasena.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO jogos (numeros) VALUES (?)', (' '.join(map(str, jogo)),))
+    conn.commit()
+    conn.close()
+
+def check_duplicate(jogo):
+    conn = sqlite3.connect('megasena.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT COUNT(*) FROM jogos WHERE numeros = ?', (' '.join(map(str, jogo)),))
+    count = cursor.fetchone()[0]
+    conn.close()
+    return count > 0
+
+def imprime_lista(jogo):
+    print(', '.join(map(str, jogo)))
+
 lista = []
 jogos = []
+create_table()
+
 print('-'*30)
 print('      jogos da MEGA SENA      ')
 print('-'*30)
 quant = int(input('Quantos jogos quer fazer?: '))
 total = 1
+
 while total <= quant:
     cont = 0
     while True:
-        num = randint(1,60)
+        num = randint(1, 60)
         if num not in lista:
             lista.append(num)
-            cont +=1
-        if cont >=6:
+            cont += 1
+        if cont >= 6:
             break
     lista.sort()
-    jogos.append(lista [:])
+
+    if not check_duplicate(lista):
+        jogos.append(lista[:])
+        save_jogo(lista)
+        total += 1
+
     lista.clear()
-    total +=1
+
 print('-='*3, f'Sorteando {quant} jogos', '-='*3)
 for i, l in enumerate(jogos):
-    print(f'Jogo {i+1}: {l}')
+    imprime_lista(l)
     sleep(0.5)
+
 print('-='*5, '< Boa sorte! >', '-='*5)
-
-
-    
-
